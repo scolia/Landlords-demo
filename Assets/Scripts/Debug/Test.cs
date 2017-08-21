b8a72a60d31800a76a81c1dd213442e18374863e
scolia <scolia@qq.com>
2017-08-21 23:57:12 +0800

    添加Debug场景

---
+++ Assets/Scripts/Debug/Test.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using Landlords;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System;
+
+public class Test : MonoBehaviour {
+
+    public GameObject CreatePoint;
+
+    public List<TestCard> cards = new List<TestCard>();
+
+    public static Test Instance { get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    // Use this for initialization
+    void Start () {
+        var deck = LandlordsUtility.CreateDeck(true, false);
+        foreach(var item in deck)
+        {
+            var temp = new GameObject(item.ToString());
+            temp.transform.SetParent(CreatePoint.transform);
+            temp.AddComponent<Image>().sprite = PokerSpriteManager.Instance.GetPokerSprite(item.Suits, item.Number);
+            temp.AddComponent<TestCard>().card = item;
+        }
+	}
+	
+	public void GetCardType()
+    {
+        var c = new List<LandlordsCard>();
+        foreach(var item in cards)
+        {
+            c.Add(item.card);
+        }
+        try
+        {
+            Debug.Log(LandlordsCardChecker.GetCardType(c));
+        }
+        catch(ArgumentException)
+        {
+            Debug.Log("no card");
+        }
+        
+    }
+
+    public void Clear()
+    {
+        foreach(var item in cards)
+        {
+            item.GetComponent<Image>().color = Color.white;
+        }
+        cards.Clear();
+    }
+}
---
+++ Assets/Scripts/Debug/TestCard.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Landlords;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class TestCard : MonoBehaviour, IPointerClickHandler
+{
+
+    public LandlordsCard card;
+    public Image image;
+    bool IsClick;
+
+    private void Start()
+    {
+        image = GetComponent<Image>();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (!IsClick)
+        {
+            image.color = Color.red;
+            Test.Instance.cards.Add(this);
+            IsClick = true;
+        }
+        else
+        {
+            image.color = Color.white;
+            Test.Instance.cards.Remove(this);
+            IsClick = false;
+        }
+        
+    }
+}
